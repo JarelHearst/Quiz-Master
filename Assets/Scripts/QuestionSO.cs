11c80c9b414495e07d16f997e345355f29eda4f8
Jarel Hearst <jarel.hearst@gmail.com>
2024-04-24 21:09:55 -0400

    Scriptable Objects

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] String question = "Enter new question text here";
+}
