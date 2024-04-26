29c58f4d6d2ac0f7128959bc8182482487e602a6
Jarel Hearst <jarel.hearst@gmail.com>
2024-04-26 11:46:52 -0400

    For Loops & Swapping Sprites

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,13 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
         questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            //Had assume because we are changing the button text we are also changing the literal text in the question text, we are just changing the question, orgininal line of code: buttonText.text = questionText.GetAnswer(0);
+            //Error: 'TextMeshProUGUI' does not contain a definition for 'GetAnswer' and no accessible extension method 'GetAnswer' accepting a first argument of type 'TextMeshProUGUI' could be found (are you missing a using directive or an assembly reference?)
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            //Importing UnityEngine.UI got rid of the error similar to Line 21.
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was: \n" + correctAnswer;
+            //questionText.text = question.GetAnswer(correctAnswerIndex);
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
     }
 }
