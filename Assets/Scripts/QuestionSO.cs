51e7b2ed2b36bb87005f2e80ab584386e117353b
Jarel Hearst <jarel.hearst@gmail.com>
2024-04-26 10:47:38 -0400

    Arrays

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -8,8 +8,22 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] String question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+    //Arrays - The line of code below was incorrect, need to make an int value instead of an array of strings, it probabbly would be right, need to test later.
+    //[SerializeField] string[] correctAnswer = new string[1];
     public string GetQuestion()
     {
         return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
     }
 }
