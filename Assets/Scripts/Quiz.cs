a78b48a2403c9084548a250400e77b23bddc9956
Jarel Hearst <jarel.hearst@gmail.com>
2024-04-27 21:38:42 -0400

    Button States

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
@@ -13,14 +14,8 @@ public class Quiz : MonoBehaviour
     [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
-        questionText.text = question.GetQuestion();
-        for (int i = 0; i < answerButtons.Length; i++)
-        {
-            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            //Had assume because we are changing the button text we are also changing the literal text in the question text, we are just changing the question, orgininal line of code: buttonText.text = questionText.GetAnswer(0);
-            //Error: 'TextMeshProUGUI' does not contain a definition for 'GetAnswer' and no accessible extension method 'GetAnswer' accepting a first argument of type 'TextMeshProUGUI' could be found (are you missing a using directive or an assembly reference?)
-            buttonText.text = question.GetAnswer(i);
-        }
+        GetNextQuestion();
+        //DisplayQuestion();
     }
 
     public void OnAnswerSelected(int index)
@@ -41,6 +36,45 @@ public class Quiz : MonoBehaviour
             //questionText.text = question.GetAnswer(correctAnswerIndex);
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+        }
+        SetButtonState(false);
+    }
+
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+    void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            //Had assume because we are changing the button text we are also changing the literal text in the question text, we are just changing the question, orgininal line of code: buttonText.text = questionText.GetAnswer(0);
+            //Error: 'TextMeshProUGUI' does not contain a definition for 'GetAnswer' and no accessible extension method 'GetAnswer' accepting a first argument of type 'TextMeshProUGUI' could be found (are you missing a using directive or an assembly reference?)
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    void SetDefaultButtonSprites()
+    {
+        Image buttonImage;
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
         }
     }
 }
